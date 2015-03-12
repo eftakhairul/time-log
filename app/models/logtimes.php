9013b73fdf6d1348711441f8d7a4c29369f3f676
Eftakhairul Islam <eftakhairul@gmail.com>
2015-03-12 11:58:36 -0400

    getWeeklyData method added to logtimes model

--- app/models/logtimes.php
+++ app/models/logtimes.php
@@ -14,6 +14,12 @@ class Logtimes extends MY_Model
     }
 
 
+    /**
+     * Return all entries based filter parameter
+     *
+     * @param array $options
+     * @return mixed
+     */
     public function getAll($options = array())
     {
         $fields = "{$this->table}.{$this->primaryKey},
@@ -25,6 +31,32 @@ class Logtimes extends MY_Model
                    teams.title as team_title";
 
         $this->db->select($fields);
+        $this->_setQueryParts($options);
+
+        $this->db->orderby("{$this->table}.date ASC");
+        $this->db->limit($this->config->item('rowsPerPage'), $options['page']);
+
+        return $this->db->get()->result_array();
+    }
+
+    /**
+     * Return only this week data
+     *
+     * @param array $options
+     * @return mixed
+     */
+    public function getWeeklyData($options = array())
+    {
+        $fields = "{$this->table}.{$this->primaryKey},
+                   {$this->table}.note,
+                   {$this->table}.date,
+                   {$this->table}.hours,
+                   {$this->table}.mins,
+                   projects.title as project_title,
+                   teams.title as team_title";
+
+        $this->db->select($fields);
+        $this->db->where("YEARWEEK(`date`, 1) = YEARWEEK(CURDATE(), 1)");
         $this->_setQueryParts($options);
 
         $this->db->orderby("{$this->table}.date ASC");
